d6073d808c78b2c3233fea7ba57d05d0088a6a08
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-21 10:12:10 +0100

    correctly delete the tmp folder

--- data/download.sh
+++ data/download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Downloads the sample file and creates a root and no root sample.
+# (Run in the data folder)
 set -euxo pipefail
 url="https://zenodo.org/records/13305156/files/20200812-CardiomyocyteDifferentiation14-Cycle1.zarr.zip?download=1"
 wget -O test.with_root.zip "$url"
@@ -13,4 +14,5 @@ cd tmp/*.zarr
 zip -r ../../test.without_root.zip *
 
 # remove the tmp folder
+cd ../..
 rm -rf tmp
